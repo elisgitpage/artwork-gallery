d63e481eb74273d035412527b78ae466d3609136
eabidor <eabidor@gmail.com>
2019-06-18 23:58:42 -0400

    Added Artworks Detail view

--- src/ArtworkGallery/Controllers/ArtworksController.cs
+++ src/ArtworkGallery/Controllers/ArtworksController.cs
@@ -10,13 +10,7 @@ namespace ArtworkGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-
-            }
-
-            return Content("Hello from the artworks controller!");
+            return View();
         }
     }
 }
