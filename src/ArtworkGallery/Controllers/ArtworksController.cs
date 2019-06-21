ad59988a49f01fa032c775c723c042da0bcc82a4
eabidor <eabidor@gmail.com>
2019-06-21 17:46:44 -0400

    Updated artwork  detail view

--- src/ArtworkGallery/Controllers/ArtworksController.cs
+++ src/ArtworkGallery/Controllers/ArtworksController.cs
@@ -8,8 +8,19 @@ namespace ArtworkGallery.Controllers
 {
     public class ArtworksController : Controller
     {
+
         public ActionResult Detail()
         {
+            ViewBag.ArtpieceTitle = " St Basils Cathedral";
+            ViewBag.Medium = "pencil on paper";
+            ViewBag.Description = "<p>Drawing of St Basils cathedral in Moscow.</p>";
+            ViewBag.Pencils = new string[]
+            {
+            "2B",
+            "4B",
+            "2H"
+            };
+
             return View();
         }
     }
