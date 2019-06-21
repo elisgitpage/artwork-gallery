18729676858ca64abd827020de95837bf339bfbc
eabidor <eabidor@gmail.com>
2019-06-21 18:21:37 -0400

    Added Artwork and Pencil models

---
+++ src/ArtworkGallery/Models/Artwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtworkGallery.Models
+{
+    public class Artwork
+    {
+        public int Id { get; set; }
+        public string ArtworkTitle { get; set; }
+        public string Medium { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Pencil[] Pencils { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
---
+++ src/ArtworkGallery/Models/Pencil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtworkGallery.Models
+{
+    public class Pencil
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
