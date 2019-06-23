c8a0602b39369780ba62fe66ab6c5a4ba349146d
eabidor <eabidor@gmail.com>
2019-06-22 23:32:15 -0400

    Updated Artworks detail view to be strongly typed

--- src/ArtworkGallery/Controllers/ArtworksController.cs
+++ src/ArtworkGallery/Controllers/ArtworksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArtworkGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,17 +12,21 @@ namespace ArtworkGallery.Controllers
 
         public ActionResult Detail()
         {
-            ViewBag.ArtpieceTitle = " St Basils Cathedral";
-            ViewBag.Medium = "pencil on paper";
-            ViewBag.Description = "<p>Drawing of St Basils cathedral in Moscow.</p>";
-            ViewBag.Pencils = new string[]
+            var artwork = new Artwork()
             {
-            "2B",
-            "4B",
-            "2H"
+                ArtworkTitle = "St Basils Cathedral",
+                Medium = "Pencil on Paper",
+                DescriptionHtml = "<p>Drawing of St Basils cathedral in Moscow.</p>",
+                Pencils = new Pencil[]
+                {
+                        new Pencil() { Name = "Graphite", Type = "2B" },
+                        new Pencil() { Name = "Watercolor", Type = "4B" },
+                        new Pencil() { Name = "Charcoal", Type = "4H" },
+                
+                }
             };
 
-            return View();
+            return View(artwork);
         }
     }
 }
