85f039dcbf1929997f45df8d9040d9c4030839ad
eabidor <eabidor@gmail.com>
2019-06-26 13:31:59 -0400

    Added artwork repository

---
+++ src/ArtworkGallery/Data/ArtworkRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ArtworkGallery.Models;
+
+namespace ArtworkGallery.Data
+{
+    public class ArtworkRepository
+    {
+        private static Artwork[] _artworks = new Artwork[]
+        {
+            new Artwork()
+            {
+                ArtworkTitle = "Paris Opera House",
+                Medium = "pencil",
+                DescriptionHtml = "<p>Drawing of Paris Opera House.</p>",
+                Pencils = new Pencil[]
+                {
+                        new Pencil() { Name = "Graphite", Type = "2B" },
+                        new Pencil() { Name = "Watercolor", Type = "4B" },
+                        new Pencil() { Name = "Charcoal", Type = "4H" },
+
+                },
+                Favorite = true
+            },
+            new Artwork()
+            {
+                ArtworkTitle = "Ink Portrait #1",
+                Medium = "Ink",
+                DescriptionHtml = "<p>Drawing of a face I found on reddit gets drawn using pen.</p>",
+                Pencils = new Pencil[]
+                {
+                        new Pencil() { Name = "Pen", Type = "Thin marker" }
+                },
+                Favorite = true
+            }
+        };
+           
+        public Artwork GetArtwork(int id)
+        {
+            Artwork artworkReturned = null;
+
+            foreach (var artWork in _artworks)
+            { 
+                if(artWork.Id == id)
+                {
+                    artworkReturned = artWork;
+
+                    break;
+                }
+            }
+
+            return artworkReturned;
+        }
+    }
+}
